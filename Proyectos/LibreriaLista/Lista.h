7632ae79fb1d9a8423e381eb2973b5455031ff69
JJaraSas <49918831+JJaraSas@users.noreply.github.com>
2019-06-03 14:29:31 -0500

    Update Lista.h
    Corrección errores

--- Proyectos/LibreriaLista/Lista.h
+++ Proyectos/LibreriaLista/Lista.h
@@ -1,6 +1,8 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+using namespace std;
+
 template <class T>
 struct nodo {
 	T dato;
@@ -28,13 +30,37 @@ class lista{
 };
 		
 template <class T>
+void lista<T>::insertar_nodo(int pos, T D){
+	int cont;
+	if (pos > tam && pos != 1){
+		insertar_final(D);
+	}else if(pos == 1 && tam < 1){
+		insertar_inicio(D);
+	}else{
+		nodo<T> *nuevo, *aux;
+		nuevo = new nodo<T>;
+		nuevo -> dato = D;
+		aux = cabeza;
+		cont = 1;
+		while(cont < pos-1 && aux!=NULL){
+			aux = aux -> sig;
+			cont++;				
+		}
+		nuevo -> sig = aux -> sig;
+		aux -> sig = nuevo;
+		tam++;
+	}
+}
+
+template <class T>
 void lista<T>::insertar_final(T D){
 	int cont;
 	nodo<T> *nuevo, *aux;
 	nuevo = new nodo<T>;
-	nuevo -> Dato = D;
+	nuevo -> dato = D;
 	nuevo -> sig = NULL;
-	aux = cabeza; 
+	aux = cabeza;
+	cont = 1;
 	while(cont < tam && aux!=NULL){
 		aux = aux -> sig;
 		cont++;				
@@ -47,33 +73,28 @@ template <class T>
 void lista<T>::insertar_inicio(T D){
 	nodo<T> *nuevo;
 	nuevo = new nodo<T>;
-	nuevo -> Dato = D;
+	nuevo -> dato = D;
 	nuevo -> sig = cabeza;
 	cabeza = nuevo;
-	cabeza++;
+	tam++;
 }
 
 template <class T>
-void lista<T>::insertar_nodo(int pos, T D){
+T lista<T>::buscar(int pos){
 	int cont;
-	if (pos > tam){
-		insertar_final(D);
-	}else if(pos == 1){
-		insertar_inicio(D);
-	}else{
-		nodo<T> *nuevo, *aux;
-		nuevo = new nodo<T>;
-		nuevo -> Dato = D;
-		aux = cabeza;
-		cont = 1;
-		while(cont < pos-1 && aux!=NULL){
-			aux = aux -> sig;
-			cont++;				
-		}
-		nuevo -> sig = aux -> sig;
-		aux -> sig = nuevo;
-		tam++;
+	nodo<T> *aux = NULL;
+	aux = cabeza;
+	cont = 1;
+	while(cont <= pos-1 && aux!=NULL){
+		aux = aux -> sig;
+		cont++;			
 	}
+	return aux -> dato;
+}
+
+template <class T>
+int lista<T>::getTam(){
+	return tam;
 }
 
 #endif
