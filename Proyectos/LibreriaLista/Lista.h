5428d8b2a30f64a261821a398b0aec1028b7e79b
nicolas240 <nicolasmadero24@gmail.com>
2019-06-02 19:22:16 -0500

    Carpetas de proyectos en dev 4.9

---
+++ Proyectos/LibreriaLista/Lista.h
@@ -0,0 +1,25 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+template <class T>
+struct nodo{
+	T dato;
+	nodo <T> *sig;
+	};
+
+class lista{
+	public:
+		lista(){cab=NULL, tam=0}
+		~lista(){}
+	private:
+		nodo<T> *cab;
+		int tam;
+		vacia();
+		void insertarNodo(int posicion, T dato);
+		void borrarNodo(int posicion);
+		T buscar(int posicion);
+		void Cambiar(int posicion, T dato);
+		int getTam();
+};
+
+#endif
