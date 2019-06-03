4961dd066dac54164d948ef574ed1882365f3acf
JJaraSas <49918831+JJaraSas@users.noreply.github.com>
2019-06-03 14:29:56 -0500

    Update main.cpp
    Prueba 1

--- Proyectos/LibreriaLista/main.cpp
+++ Proyectos/LibreriaLista/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
-#include "Lista.h"
+#include <clocale>
+#include "lista.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+using namespace std;
+int main(){
+	setlocale(LC_CTYPE,"Spanish");
+	lista <int> x;
+	lista <float> y;
+	lista <char> z;
+	
+	cout << "Tamaño inicial: " << x.getTam() << endl;
+	
+	cout << "Insertando primer nodo..." << endl;
+	x.insertar_nodo(1, 8);
+
+	cout << "------------------------" << endl;
+	cout << "Insertando segundo nodo..." << endl;
+	x.insertar_nodo(1, 100);
+	
+	cout << "------------------------" << endl;
+	cout << "Insertando tercer nodo..." << endl;
+	x.insertar_nodo(3, 400);
+	
+	cout << "------------------------" << endl;
+	cout << "Buscando valor pos = 1: ";
+	cout << x.buscar(1) << endl;
+	cout << "Buscando valor pos = 2: ";
+	cout << x.buscar(2) << endl;
+	cout << "Buscando valor pos = 3: ";
+	cout << x.buscar(3) << endl;
+	
+	cout << "Tamaño al terminar: " << x.getTam() << endl;
+	
 	return 0;
 }
