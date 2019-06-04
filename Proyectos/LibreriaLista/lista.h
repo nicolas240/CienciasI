549fd828f36ebab0584d9140ec1fc59dbb289525
nicolas240 <nicolasmadero24@gmail.com>
2019-06-03 19:50:09 -0500

    lista.h y main.cpp

---
+++ Proyectos/LibreriaLista/lista.h
@@ -0,0 +1,158 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+struct nodo {
+	T dato;
+	nodo <T> *sig;
+};
+
+template <class T>
+class lista{
+	private:
+		nodo <T> *cabeza;
+		int tam;
+	
+	public:
+		lista(){
+			cabeza = NULL; 
+			tam = 0;
+		}
+		~lista(){
+			int cont=1;
+			nodo<T> *recorre, *aux=cabeza;
+			while(cont < tam && aux!=NULL){
+			recorre=aux;
+			aux = aux -> sig;
+			delete recorre;
+			cont++;				
+			}
+		}
+		void insertar_nodo(int pos, T D);
+		void insertar_final(T D);
+		void insertar_inicio(T D);
+		void borrar_nodo(int pos);
+		void modificar(int pos, T D);
+		int getTam();
+		bool esVacia();
+		T buscar(int pos);
+};
+		
+template <class T>
+void lista<T>::insertar_nodo(int pos, T D){
+	int cont;
+	if (pos > tam && pos != 1){
+		insertar_final(D);
+	}else if(pos == 1 && tam < 1){
+		insertar_inicio(D);
+	}else{
+		nodo<T> *nuevo, *aux;
+		nuevo = new nodo<T>;
+		nuevo -> dato = D;
+		aux = cabeza;
+		cont = 1;
+		while(cont < pos-1 && aux!=NULL){
+			aux = aux -> sig;
+			cont++;				
+		}
+		nuevo -> sig = aux -> sig;
+		aux -> sig = nuevo;
+		tam++;
+	}
+}
+
+template <class T>
+void lista<T>::insertar_final(T D){
+	int cont;
+	nodo<T> *nuevo, *aux;
+	nuevo = new nodo<T>;
+	nuevo -> dato = D;
+	nuevo -> sig = NULL;
+	aux = cabeza;
+	cont = 1;
+	while(cont < tam && aux!=NULL){
+		aux = aux -> sig;
+		cont++;				
+	}
+	aux -> sig = nuevo;
+	tam++;
+}
+
+template <class T>
+void lista<T>::insertar_inicio(T D){
+	nodo<T> *nuevo;
+	nuevo = new nodo<T>;
+	nuevo -> dato = D;
+	nuevo -> sig = cabeza;
+	cabeza = nuevo;
+	tam++;
+}
+
+template <class T>
+T lista<T>::buscar(int pos){
+	int cont;
+	nodo<T> *aux = NULL;
+	aux = cabeza;
+	cont = 1;
+	while(cont <= pos-1 && aux!=NULL){
+		aux = aux -> sig;
+		cont++;			
+	}
+	return aux -> dato;
+}
+
+template <class T>
+void lista<T>::borrar_nodo(int pos){
+	int cont;
+	nodo<T> *aux = NULL;
+	nodo<T> *aux2 = NULL;
+	aux = cabeza;
+	cont = 1;
+	while(cont < pos -1 && aux!=NULL){
+		aux = aux -> sig;
+		cont++;			
+	}
+	if(tam!=1 || cabeza !=NULL){
+		if(pos == 1){
+			cabeza = aux -> sig;
+			delete aux;
+		}else{
+			aux2 = aux -> sig;
+			aux -> sig = aux2 -> sig;
+			delete aux2;
+		}
+		tam--;
+	}
+}
+
+template <class T>
+void lista<T>::modificar(int pos, T D){
+	int cont=1;
+	nodo<T> *aux= cabeza;
+	while(cont < pos -1 && aux!=NULL){
+		aux = aux -> sig;
+		cont++;
+	}
+	if(pos==1)
+		cabeza->dato=D;
+	else
+		aux->sig->dato=D;
+}
+
+template <class T>
+bool lista<T>::esVacia(){
+	if(cabeza->sig==NULL)
+		return true;
+	else
+		return false;
+}
+
+template <class T>
+int lista<T>::getTam(){
+	return tam;
+}
+#endif
--- Proyectos/LibreriaLista/main.cpp
+++ Proyectos/LibreriaLista/main.cpp
@@ -10,7 +10,7 @@ int main(){
 	lista <float> y;
 	lista <char> z;
 	
-	cout << "TamaÃ±o inicial: " << x.getTam() << endl;
+	cout << "Tamaño inicial: " << x.getTam() << endl;
 	
 	cout << "Insertando primer nodo..." << endl;
 	x.insertar_nodo(1, 8);
@@ -23,6 +23,7 @@ int main(){
 	cout << "Insertando tercer nodo..." << endl;
 	x.insertar_nodo(3, 400);
 	
+	cout<<"¿Es vacia la lista?:"<<x.esVacia()<<endl;
 	cout << "------------------------" << endl;
 	cout << "Buscando valor pos = 1: ";
 	cout << x.buscar(1) << endl;
@@ -30,15 +31,34 @@ int main(){
 	cout << x.buscar(2) << endl;
 	cout << "Buscando valor pos = 3: ";
 	cout << x.buscar(3) << endl;
-	
-	cout << "TamaÃ±o al terminar: " << x.getTam() << endl;
+	cout << "Tamaño al terminar: " << x.getTam() << endl;
 	
 	cout << "------------------------" << endl;
 	cout << "Borrando dato 1..." << endl;
 	x.borrar_nodo(1);
-	
 	cout << "Nuevo valor pos = 1: ";
 	cout << x.buscar(1) << endl;
 	
+	cout << "------------------------" << endl;
+	cout << "Cambiando dato 1..." << endl;	
+	x.modificar(1,500);	
+	cout << "Nuevo valor pos = 1: ";
+	cout << x.buscar(1) << endl;
+	
+	cout << "------------------------" << endl;
+	x.borrar_nodo(1);
+	cout<<"¿Es vacia la lista?:"<<x.esVacia()<<endl;
+	cout << "Buscando valor pos = 1: ";
+	cout << x.buscar(1) << endl;
+	/*cout << "Buscando valor pos = 2: ";
+	cout << x.buscar(2) << endl;
+	cout << "Buscando valor pos = 3: ";
+	cout << x.buscar(3) << endl;*/
+	cout << "Tamaño al terminar: " << x.getTam() << endl;
+
+	x.borrar_nodo(1);
+	cout<<"¿Es vacia la lista?:"<<x.esVacia()<<endl;
+	cout << "Buscando valor pos = 1: ";
+	cout << "Tamaño al terminar: " << x.getTam() << endl;
 	return 0;
 }
