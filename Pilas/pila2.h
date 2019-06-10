5fea8ea86aa5bff11ce618fb46d2b7b38992f63c
JJaraSas <49918831+JJaraSas@users.noreply.github.com>
2019-06-10 13:53:03 -0500

    Create pila2.h

---
+++ Pilas/pila2.h
@@ -0,0 +1,31 @@
+#ifndef PILA_H
+#define PILA_H
+using namespace std;
+
+struct nodo{
+	char clave; struct nodo *siguiente;
+};
+
+class Pila{
+	char *pila;
+  	int p;
+public:
+		
+	Pila (int max=100){
+		pila = new char[max]; p=0;
+	}
+	~Pila(){
+		delete pila;
+	}
+	inline void meter (char v){
+		pila[p++]=v;
+	}
+	inline char sacar(){
+		return pila [--p];
+	}
+	inline int vacia(){
+		return !p;
+	} 
+};
+
+#endif
