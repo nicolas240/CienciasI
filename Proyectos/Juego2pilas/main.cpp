55b8fb5cc96a9a2655aea42a0f08a264dfd9cb86
nicolas240 <nicolasmadero24@gmail.com>
2019-06-16 21:21:01 -0500

    juego pilas

--- Proyectos/Juego2pilas/main.cpp
+++ Proyectos/Juego2pilas/main.cpp
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
     //ios_base::sync_with_stdio(false);
     //wcout.imbue(locale("es_US.UTF-8"));
 
-	cola Cola; 
+	cola Cola;
 	pila Pila[2]; 
-	int i,j, tam, grupo=0, orden[2];
+	int i,j,ord, tam, grupo=0, orden[2];
 	char x; 
   
 	do{
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
    		cin>> tam;}
 	while (tam%2!=0);
  	i=1;
-	x=0;
+	x=32;
 	cout<<x;
 	while(i<=tam && i<255)
 	{	if((x==0)||(x==13)||(x==32)||(x>6&&x<11))
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	}
 	cout<<"mire cuidadosamente los sìmbolos y elija uno\n";
 	
-	for(j=1; j<tam/2; j++)
+	for(j=1; j<=tam/2; j++)
 	{
 		cout<<"  Gr1  "<<"Gr2  "<<endl;
 	  	while (!Cola.ColaVacia())
@@ -52,18 +52,26 @@ int main(int argc, char *argv[])
 	   	cout<< "elija el grupo en el que quedò el sìmbolo elegido (1, 2): ";
 	  	cin>> grupo;
 	  	switch(grupo){
-	    	case 1 : {orden[0]=1; orden[1]=0; break;}
-	        case 2 : {orden[0]=0; orden[1]=1; break;}
+	    	case 1 : {ord=1; orden[0]=1; orden[1]=0; break;}
+	        case 2 : {ord=0; orden[0]=0; orden[1]=1; break;}
 			default: {cout<<"Grupo invalido"<<endl;	break;}       
 	    }
-	 	 for (i=0; i<2; i++)
-	    {
+	 	 for (i=0; i<2; i++){
 			while (!Pila[orden[i]].PilaVacia())
 	        {Cola.InsCola(Pila[orden[i]].Pop());}
 	    }
 	}
-	j=tam/2+1;
-	
+	if(ord==0 and tam>4 ){
+		 j++;
+		if(tam>8) j+=tam/8;
+	}
+	if(ord==1){
+		if(tam>8) j+=tam/8;	
+	}
+	/*cout <<"ord :"<<ord<<endl;
+	cout<<"j : "<<j<<endl;
+	cout<<"tam :"<<tam<<endl;
+	Cola.ImprimirCola();*/	
 	for (i=1; i<=j; i++)
 	{x=Cola.AtenderCola();}
 	cout<<"La figura elegida es: "<< x<<endl; 
